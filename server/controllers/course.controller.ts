8b29e09be124ecc7a0be3c0847ec43cbb7cb5e07
iamkibet <100912353+iamkibet@users.noreply.github.com>
2024-03-18 20:23:32 +0300

    Validated users gets course

--- server/controllers/course.controller.ts
+++ server/controllers/course.controller.ts
@@ -126,3 +126,31 @@ export const getAllCourses = catchAsyncError(
     }
   }
 );
+
+//get course by valid user
+export const getCourseByUser = catchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const userCourseList = req.user?.courses;
+      const courseId = req.params.id;
+      const courseExists = userCourseList?.find(
+        (course: any) => course._id.toString() === courseId
+      );
+      if (!courseExists) {
+        return next(
+          new ErrorHandler("You are not eligible to access this course", 404)
+        );
+      }
+      const course = await CourseModel.findById(courseId);
+
+      const content = course?.courseData;
+
+      res.status(200).json({
+        success: true,
+        content,
+      });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  }
+);
