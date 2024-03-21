cdb943ff4f4ab7c44741ddd9146279ea1af72f77
iamkibet <100912353+iamkibet@users.noreply.github.com>
2024-03-21 17:13:31 +0300

    Create Order and Notification Models

---
+++ server/models/notification.model.ts
@@ -0,0 +1,34 @@
+import mongoose, { Document, Model, Schema } from "mongoose";
+
+export interface INotification extends Document {
+  title: string;
+  message: string;
+  status: string;
+  userId: string;
+}
+
+const notificationSchema = new Schema<INotification>(
+  {
+    title: {
+      trype: String,
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      default: "unread",
+    },
+  },
+  { timestamps: true }
+);
+
+const notificationModel: Model<INotification> = mongoose.model(
+  "Notification",
+  notificationSchema
+);
+
+export default notificationModel;
